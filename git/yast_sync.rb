ca37c6ac95b26f3f914591c1906fc885b29f8424
Ladislav Slezák <lslezak@suse.cz>
2015-10-08 10:35:11 +0200

    yast_sync.rb - run the sync in parallel
    it's about 4-5 times faster...

--- git/yast_sync.rb
+++ git/yast_sync.rb
@@ -1,6 +1,8 @@
 #!/usr/bin/env ruby
 
 require "json"
+require "shellwords"
+require_relative "threading"
 
 # cache the read repos from Github
 CACHE_FILE = ".yast_repos_cache.json"
@@ -22,59 +24,78 @@ def read_yast_cache
   repos
 end
 
+def run_in(dir, cmd)
+  attempt = 1
+
+  loop do
+    `(cd #{Shellwords.escape(dir)} && #{cmd})`
+
+    break if $?.success?
+
+    if attempt > 5
+      $stderr.puts "Command #{cmd.inspect} still fails, giving up"
+      exit(1)
+    end
+
+    attempt += 1
+    $stderr.puts "Error: #{$?.exitstatus}, retrying in 10 seconds..."
+    sleep(10)
+  end
+end
+
 
 repos = read_yast_cache
 puts "Found #{repos.size} Yast repositories"
 
 IGNORE = [
-"skelcd-control-SLES-for-VMware",
-"rubygem-scc_api",
-"yast-backup",
-"yast-bluetooth",
-"yast-boot-server",
-"yast-cd-creator",
-"yast-certify",
-"yast-cim",
-"yast-databackup",
-"yast-dbus-client",
-"yast-debugger",
-"yast-dialup",
-"yast-dirinstall",
-"yast-fax-server",
-"yast-fingerprint-reader",
-"yast-heartbeat",
-"yast-hpc",
-"yast-ipsec",
-"yast-irda",
-"yast-liby2util",
-"yast-meta",
-"yast-mouse",
-"yast-mysql-server",
-"yast-ntsutils",
-"yast-oem-installation",
-"yast-online-update-test",
-"yast-openschool",
-"yast-openteam",
-"yast-openwsman-yast",
-"yast-packagemanager",
-"yast-packagemanager-test",
-"yast-phone-services",
-"yast-power-management",
-"yast-profile-manager",
-"yast-repair",
-"yast-restore",
-"yast-squidguard",
-"yast-sshd",
-"yast-sudo",
-"yast-support",
-"yast-system-profile",
-"yast-system-update",
-"yast-ui-qt-tests",
-"yast-uml",
-"yast-you-server",
-"yast-yxmlconv",
-"yast-y2pmsh",
-"yast-y2r-tools",
+  "skelcd-control-SLES-for-VMware",
+  "rubygem-scc_api",
+  "yast-backup",
+  "yast-bluetooth",
+  "yast-boot-server",
+  "yast-cd-creator",
+  "yast-certify",
+  "yast-cim",
+  "yast-databackup",
+  "yast-dbus-client",
+  "yast-debugger",
+  "yast-dialup",
+  "yast-dirinstall",
+  "yast-fax-server",
+  "yast-fingerprint-reader",
+  "yast-heartbeat",
+  "yast-hpc",
+  "yast-ipsec",
+  "yast-irda",
+  "yast-liby2util",
+  "yast-meta",
+  "yast-mouse",
+  "yast-mysql-server",
+  "yast-ntsutils",
+  "yast-oem-installation",
+  "yast-online-update-test",
+  "yast-openschool",
+  "yast-openteam",
+  "yast-openwsman-yast",
+  "yast-packagemanager",
+  "yast-packagemanager-test",
+  "yast-phone-services",
+  "yast-power-management",
+  "yast-profile-manager",
+  "yast-repair",
+  "yast-restore",
+  "yast-squidguard",
+  "yast-sshd",
+  "yast-sudo",
+  "yast-support",
+  "yast-system-profile",
+  "yast-system-update",
+  "yast-ui-qt-tests",
+  "yast-uml",
+  "yast-you-server",
+  "yast-yxmlconv",
+  "yast-y2pmsh",
+  "yast-y2r-tools",
 ]
 
 repos = repos - IGNORE
@@ -82,26 +103,22 @@ puts "Ignoring #{IGNORE.size} obsoleted repositories, using #{repos.size} reposi
 
 repos.sort!
 
-repos.each_with_index do |repo, index|
+Threading.in_parallel(repos) do |repo|
   dir = repo.sub(/^yast-/, "")
 
   if File.exist?(dir)
-    puts "[#{index + 1}/#{repos.size}] Updating #{dir}..."
+    puts "Updating #{dir}..."
 
-    Dir.chdir dir do
-      `git reset --hard`
-      `git checkout -q master`
-      `git fetch --prune`
-      `git pull --rebase`
-    end
+    run_in(dir, "git reset --hard")
+    run_in(dir, "git checkout -q master")
+    run_in(dir, "git fetch --prune")
+    run_in(dir, "git pull --rebase")
   else
-    puts "[#{index + 1}/#{repos.size}] Cloning #{dir}..."
-    `git clone git@github.com:yast/#{repo}.git #{dir}`
+    puts "Cloning #{dir}..."
+    run_in(Dir.pwd, "git clone git@github.com:yast/#{repo}.git #{dir}")
   end
 
   # add your code here to run it in each git repo:
-  # Dir.chdir repo do
-  #   `your command here`
-  # end
+  # run_in(dir, "cmd")
 end
 
