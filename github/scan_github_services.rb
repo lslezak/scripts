16d51e3e206f581bd8a2c8e7d19aa748a3fb715b
Ladislav Slezák <lslezak@suse.cz>
2019-01-11 12:48:16 +0100

    Print failed hook checks

--- github/scan_github_services.rb
+++ github/scan_github_services.rb
@@ -16,27 +16,35 @@ require "bundler/setup"
 require "octokit"
 
 # organization/user to scan
-organization = "libyui"
+organization = "yast"
 
 # uncomment for debugging, enable the gem also in Gemfile
 # require "byebug"
 # byebug
 
-
 # read GitHub repos
 client = Octokit::Client.new(access_token: ENV["GH_TOKEN"])
 client.auto_paginate = true
 
-print "Reading GitHub repositories..."
+print "Reading #{organization} GitHub repositories..."
 git_repos = client.list_repositories(organization)
 puts " done, #{git_repos.size} repositories found"
 
-
 obsolete_hooks = []
+failed_repos = []
 
 git_repos.each do |repo|
-  puts "Loading #{repo.full_name} hooks..."
-  hooks = client.hooks(repo.full_name)
+  print "Loading #{repo.full_name} hooks... "
+
+  begin
+    hooks = client.hooks(repo.full_name)
+  rescue
+    puts "FAILED!"
+    failed_repos << repo.full_name
+    next
+  end
+
+  puts
 
   hooks.each do |h|
     next if h.name == "web" # || h.name == "email"
@@ -45,6 +53,11 @@ git_repos.each do |repo|
   end
 end
 
-print "\nFound #{obsolete_hooks.size} obsolete hooks:\n\n"
-
+print "\nFound #{obsolete_hooks.size} obsolete services:\n\n"
 puts obsolete_hooks.sort
+
+if !failed_repos.empty?
+  puts "\n#{failed_repos.size} repositories failed (missing permission?):"
+  puts failed_repos.join(", ")
+  exit 1
+end
